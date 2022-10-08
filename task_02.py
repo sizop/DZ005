cf801b73e7c6b837394806bb5fee1fbcea5f7e2c
Dmirty <9048881951@mail.ru>
2022-10-08 17:48:58 +0500

    08/10

---
+++ task_02.py
@@ -0,0 +1,24 @@
+# 2. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.Входные и выходные
+# данные хранятся в отдельных текстовых файлах.
+# Алгоритм RLE
+#
+# in
+# Enter the name of the file with the text:
+# 'text_words.txt'
+# Enter the file name to record:
+# 'text_code_words.txt'
+# Enter the name of the file to decode:
+# 'text_code_words.txt'
+#
+# out
+# aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
+# vvvvvvvvvvvbbwwPPuuuTTYyWWQQ
+#
+# out in file
+# 'text_words.txt'
+# aaaaavvvvvvvvvvvvvvvvvvvvvvvvvvvvvssssDDDdddFFggggOOiiiaa
+# vbbwwPPuuuTTYyWWQQ
+#
+# 'text_code_words.txt'
+# 5a29v4s3D3d2F4g2O3i2a1
+# 1v2b2w2P3u2T1Y1y2W2Q
